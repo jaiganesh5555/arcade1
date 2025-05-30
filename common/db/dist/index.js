8a564a56e9106e853a05cf0f86a17a588ba6bb20
jaiganesh5555 <142227919+jaiganesh5555@users.noreply.github.com>
2025-05-30 23:35:29 +0530

    Initial commit 🚀

--- common/db/dist/index.js
+++ common/db/dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PrismaClient = exports.prisma = void 0;
 const client_1 = require("@prisma/client");
-Object.defineProperty(exports, "PrismaClient", { enumerable: true, get: function () { return client_1.PrismaClient; } });
-exports.prisma = new client_1.PrismaClient();
+// Create a single instance of PrismaClient
+const prisma = new client_1.PrismaClient();
+// Export the instance as default
+exports.default = prisma;
