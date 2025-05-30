d621098e6ba7644765b56abe4a716b16261f728a
jaiganesh5555 <142227919+jaiganesh5555@users.noreply.github.com>
2025-05-30 23:33:10 +0530

    Initial commit 🚀

--- common/db/index.ts
+++ common/db/index.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
+// Create a single instance of PrismaClient
 const prisma = new PrismaClient();
 
+// Export the instance as default
 export default prisma;
-export { PrismaClient }; 
+
+// Also export the type for type checking
+export type { PrismaClient }; 
