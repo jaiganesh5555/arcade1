51e2ba4c174dab949ada5b652ae9a46134cc2a4c
jaiganesh5555 <142227919+jaiganesh5555@users.noreply.github.com>
2025-06-01 09:37:49 +0530

    GSD

--- common/db/dist/index.js
+++ common/db/dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.PrismaClient = void 0;
 const client_1 = require("@prisma/client");
+Object.defineProperty(exports, "PrismaClient", { enumerable: true, get: function () { return client_1.PrismaClient; } });
 // Create a single instance of PrismaClient
 const prisma = new client_1.PrismaClient();
-// Export the instance as default
+// Export the instance
 exports.default = prisma;
